41456c2d4f77b82de9c9aa6008626fe1f1b288da
alexbar83 <115842630+alexbar83@users.noreply.github.com>
2024-10-22 19:17:30 +0200

    files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,6 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-
-
 ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../config/environment', __dir__)
-
+require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -24,7 +19,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
- Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -35,10 +30,12 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.include ControllerHelpers, type: :controller
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerHelpers, type: :controller
   config.include FeatureHelpers, type: :feature
+
+  Capybara.javascript_driver = :selenium_chrome_headless
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -66,12 +63,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
+  config.after(:all) { FileUtils.rm_rf("#{Rails.root.join('tmp/storage')}") }
 end
-Capybara.javascript_driver = :silenium_chrome_headless
+
 Shoulda::Matchers.configure do |config|
-   config.integrate do |with|
-     with.test_framework :rspec
-     with.library :rails
-   end
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
