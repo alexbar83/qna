ea69ba594f45750bf4a2a11aa65ad67f051326c2
alexbar83 <115842630+alexbar83@users.noreply.github.com>
2024-12-25 19:49:57 +0100

    accept

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it {should validate_presecne_of :title }
   it {should validate_presecne_of :body }
+  it { should have_db_index :user_id }
 
   it { should have_many(:answers).dependent(:destroy) }
 end
